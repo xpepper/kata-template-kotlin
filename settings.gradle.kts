a07864293d377b97755794871916e3fb39400ee2
Pietro Di Bello <pierodibello@gmail.com>
2024-03-03 23:01:44 +0100

    chore(gradle): downgrade to gradle 8.5 to use kotlin 1.9.21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-
 rootProject.name = "kata-template-kotlin"
-include("app")
